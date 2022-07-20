13c0e2ee3df8643bc4e495aee5bceb5486bf77fb
Julia <juliam.sec@gmail.com>
2022-07-19 21:41:38 -0400

    Julia Fiest Commit

--- cat.js
+++ cat.js
@@ -1,16 +1,16 @@
-const ls = require('./ls');
-const fs = require('fs');
+const fs = require('fs')
 //Output a prompt
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim(); //remove the newline
-  if(cmd === 'pwd'){
-    process.stdout.write(process.cwd());
+  const cmd = data.toString().trim().split(' '); //remove the newline
+  
+  if(cmd[0] === 'cat'){
+    fs.read;
    }else if (cmd === 'ls') {
-	ls()
-   } 
+        ls()
+   }
  process.stdout.write('\nprompt > ');
-}); 
+});
 
